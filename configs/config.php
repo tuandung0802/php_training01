8cb01f5a89b950812d685f262b4b97bbfd87123c
Tuan Dung <dunglqd0802@gmail.com>
2021-12-11 21:17:14 +0700

    Create config.php

---
+++ configs/config.php
@@ -0,0 +1,12 @@
+<?php 
+
+define('SITE_URL', 'http://localhost/');
+
+
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'training_php_01');
+
+
+?>
